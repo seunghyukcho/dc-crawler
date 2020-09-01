14bee020822ffcb9e20d992fdb1d9d01832fe4f3
조승혁 <seunghyuk.cho@navercorp.com>
2020-09-01 17:10:59 +0900

    구현 완료

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-import requests
+from tap import Tap
+from tqdm import tqdm
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+
+class ArgumentParser(Tap):
+    save_to: str  # Path to output file
+    gallery: str  # Name of gallery (etc. leagueoflegends3)
+    start_idx: int = 1  # Start index of gallery page
+    end_idx: int  # End index of gallery page
+    chrome_driver: str  # Path to chrome driver
+
+
 if __name__ == "__main__":
+    args = ArgumentParser().parse_args()
+
+    log_file = open(args.save_to, 'w')
+
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     options.add_argument("window-size=1920x1080")
     options.add_argument("disable-gpu")
 
-    driver = webdriver.Chrome("/Users/user/chromedriver/chromedriver", options=options)
-    # driver.implicitly_wait(3)
+    driver = webdriver.Chrome(args.chrome_driver, options=options)
 
-    for page in range(1, 10):
-        driver.get(f"https://gall.dcinside.com/board/view/?id=leagueoflegends3&no={page}")
+    for page_idx in tqdm(range(args.start_idx, args.end_idx + 1)):
+        driver.get(f"https://gall.dcinside.com/board/view/?id={args.gallery}&no={page_idx}")
         try:
             driver.find_element_by_class_name("delet")
         except NoSuchElementException:
             pass
         else:
-            print(f"page #{page} not found")
             continue
 
         try:
@@ -28,11 +40,20 @@ if __name__ == "__main__":
 
             title = driver.find_element_by_class_name("title_subject")
             content = driver.find_element_by_class_name("writing_view_box")
+            content = content.find_elements_by_tag_name('div')
+            content = content[-1]
+
+            log_file.write(title.text + '\n')
+            log_file.write(content.text.replace('\n', ' ') + '\n')
 
             for i in range(last_comment_page + 1, 0, -1):
                 driver.execute_script(f"viewComments({i}, 'D')")
                 comments = driver.find_elements_by_class_name("usertxt")
+                for comment in comments:
+                    log_file.write(comment.text + '\n')
+            log_file.write("\n\n\n")
         except NoSuchElementException:
-            page -= 1
+            page_idx -= 1
 
+    log_file.close()
     driver.quit()
