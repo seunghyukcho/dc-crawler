f25419ab05b3f344cb0ffebbdb572091aac5af8f
조승혁 <seunghyuk.cho@navercorp.com>
2020-09-01 17:34:16 +0900

    logging 기능 추가 및 코드 정리

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from tap import Tap
 from tqdm import tqdm
 from selenium import webdriver
@@ -6,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 class ArgumentParser(Tap):
     save_to: str  # Path to output file
+    log_to: str = "log.txt"  # Path to log file
     gallery: str  # Name of gallery (etc. leagueoflegends3)
     start_idx: int = 1  # Start index of gallery page
     end_idx: int  # End index of gallery page
@@ -15,7 +17,8 @@ class ArgumentParser(Tap):
 if __name__ == "__main__":
     args = ArgumentParser().parse_args()
 
-    log_file = open(args.save_to, 'w')
+    logging.basicConfig(filename=args.log_to, format="%(asctime)s %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
+    save_file = open(args.save_to, 'w')
 
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
@@ -23,6 +26,7 @@ if __name__ == "__main__":
     options.add_argument("disable-gpu")
 
     driver = webdriver.Chrome(args.chrome_driver, options=options)
+    driver.implicitly_wait(5)
 
     for page_idx in tqdm(range(args.start_idx, args.end_idx + 1)):
         driver.get(f"https://gall.dcinside.com/board/view/?id={args.gallery}&no={page_idx}")
@@ -34,26 +38,30 @@ if __name__ == "__main__":
             continue
 
         try:
+            comments = []
             n_comments = driver.find_element_by_class_name("cmt_paging")
-            n_comments = n_comments.find_elements_by_tag_name('a')
+            n_comments = n_comments.find_elements_by_tag_name("a")
             last_comment_page = len(n_comments) + 1
 
             title = driver.find_element_by_class_name("title_subject")
             content = driver.find_element_by_class_name("writing_view_box")
-            content = content.find_elements_by_tag_name('div')
+            content = content.find_elements_by_tag_name("div")
             content = content[-1]
-
-            log_file.write(title.text + '\n')
-            log_file.write(content.text.replace('\n', ' ') + '\n')
 
             for i in range(last_comment_page + 1, 0, -1):
                 driver.execute_script(f"viewComments({i}, 'D')")
-                comments = driver.find_elements_by_class_name("usertxt")
-                for comment in comments:
-                    log_file.write(comment.text + '\n')
-            log_file.write("\n\n\n")
+                page_comments = driver.find_elements_by_class_name("usertxt")
+                for comment in page_comments:
+                    comments.append(comment.text)
+
+            save_file.write(title.text + '\n')
+            save_file.write(content.text.replace('\n', ' ') + '\n')
+            for comment in comments:
+                save_file.write(comment + '\n')
+            save_file.write("\n\n\n")
         except NoSuchElementException:
+            logging.warning(f"Page #{page_idx} reloading...")
             page_idx -= 1
 
-    log_file.close()
+    save_file.close()
     driver.quit()
