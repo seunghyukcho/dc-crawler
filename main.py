dc9c7b2bb10e62ef7b4de113b0d22d75b2af1740
seunghyuk.cho <shhj1998@postech.ac.kr>
2020-09-01 13:30:26 +0000

    디시 서버 장애시 상황 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from tap import Tap
 from tqdm import tqdm
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 class ArgumentParser(Tap):
@@ -13,12 +13,12 @@ class ArgumentParser(Tap):
     end_idx: int  # End index of gallery page
     chrome_driver: str  # Path to chrome driver
 
-
+    
 if __name__ == "__main__":
     args = ArgumentParser().parse_args()
 
     logging.basicConfig(filename=args.log_to, format="%(asctime)s %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
-    save_file = open(args.save_to, 'w')
+    save_file = open(args.save_to, 'a')
 
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
@@ -29,39 +29,42 @@ if __name__ == "__main__":
     driver.implicitly_wait(5)
 
     for page_idx in tqdm(range(args.start_idx, args.end_idx + 1)):
-        driver.get(f"https://gall.dcinside.com/board/view/?id={args.gallery}&no={page_idx}")
-        try:
-            driver.find_element_by_class_name("delet")
-        except NoSuchElementException:
-            pass
-        else:
-            continue
+        while True:
+            try:
+                driver.get(f"https://gall.dcinside.com/board/view/?id={args.gallery}&no={page_idx}")
 
-        try:
-            comments = []
-            n_comments = driver.find_element_by_class_name("cmt_paging")
-            n_comments = n_comments.find_elements_by_tag_name("a")
-            last_comment_page = len(n_comments) + 1
+                try:
+                    driver.find_element_by_class_name("delet")
+                except NoSuchElementException:
+                    pass
+                else:
+                    break
 
-            title = driver.find_element_by_class_name("title_subject")
-            content = driver.find_element_by_class_name("writing_view_box")
-            content = content.find_elements_by_tag_name("div")
-            content = content[-1]
+                comments = []
+                n_comments = driver.find_element_by_class_name("cmt_paging")
+                n_comments = n_comments.find_elements_by_tag_name("a")
+                last_comment_page = len(n_comments) + 1
 
-            for i in range(last_comment_page + 1, 0, -1):
-                driver.execute_script(f"viewComments({i}, 'D')")
-                page_comments = driver.find_elements_by_class_name("usertxt")
-                for comment in page_comments:
-                    comments.append(comment.text)
+                title = driver.find_element_by_class_name("title_subject")
+                content = driver.find_element_by_class_name("writing_view_box")
+                content = content.find_elements_by_tag_name("div")
+                content = content[-1]
 
-            save_file.write(title.text + '\n')
-            save_file.write(content.text.replace('\n', ' ') + '\n')
-            for comment in comments:
-                save_file.write(comment + '\n')
-            save_file.write("\n\n\n")
-        except NoSuchElementException:
-            logging.warning(f"Page #{page_idx} reloading...")
-            page_idx -= 1
+                for i in range(last_comment_page, 0, -1):
+                    driver.execute_script(f"viewComments({i}, 'D')")
+                    page_comments = driver.find_elements_by_class_name("usertxt")
+                    for comment in page_comments:
+                        comments.append(comment.text)
+
+                save_file.write(title.text + '\n')
+                save_file.write(content.text.replace('\n', ' ') + '\n')
+                for comment in comments:
+                    save_file.write(comment + '\n')
+                save_file.write("\n\n\n")
+            except (NoSuchElementException, TimeoutException):
+                logging.warning(f"Page #{page_idx} reloading...")
+            else:
+                break
 
     save_file.close()
     driver.quit()
